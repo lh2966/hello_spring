b433699501685a21d9460e83c1ec045c00f47a60
lh_2966 <bach1bach2@163.com>
2019-12-15 15:54:26 +0800

    add indenx and css

--- src/main/java/com/lhh/hello_spring/hello_world/hello_World.java
+++ src/main/java/com/lhh/hello_spring/hello_world/hello_World.java
@@ -3,16 +3,12 @@ package com.lhh.hello_spring.hello_world;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import sun.misc.Contended;
 
 @Controller
 public class hello_World {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name= "name")String name,String age, Model model){
-        model.addAttribute("name",name);
-        model.addAttribute("age",age);
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+        return "indnx";
     }
 }
