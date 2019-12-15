25e6a2e79df83bf942c345a3aea3d342cb769005
lh_2966 <bach1bach2@163.com>
2019-12-15 17:10:39 +0800

    add GitHud user

--- src/main/java/com/lhh/hello_spring/hello_world/hello_World.java
+++ src/main/java/com/lhh/hello_spring/hello_world/hello_World.java
@@ -1,14 +1,12 @@
 package com.lhh.hello_spring.hello_world;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class hello_World {
     @GetMapping("/")
     public String hello(){
-        return "indnx";
+        return "index";
     }
 }
