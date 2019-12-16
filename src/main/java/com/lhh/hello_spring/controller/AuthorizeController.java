80fcad2ff7b51b014e5c008132cd72e888209d71
lh2966 <bach1bach2@163.com>
2019-12-16 10:50:46 +0800

    add properties

--- src/main/java/com/lhh/hello_spring/controller/AuthorizeController.java
+++ src/main/java/com/lhh/hello_spring/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.lhh.hello_spring.dto.AccessTokenDTO;
 import com.lhh.hello_spring.dto.GithubUser;
 import com.lhh.hello_spring.provider.GithudProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithudProvider githudProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private  String clientSecret;
+    @Value("${github.redirect.url}")
+    private  String redirectUrl;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("dd48594715f07aa9a49b");
-        accessTokenDTO.setClient_secret("3710c6be1f7622986d6f336766b7922b6a2d7f3f");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:9527/callback");
+        accessTokenDTO.setRedirect_uri(redirectUrl);
         accessTokenDTO.setState(state);
         String accessToken = githudProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githudProvider.getUser(accessToken);
