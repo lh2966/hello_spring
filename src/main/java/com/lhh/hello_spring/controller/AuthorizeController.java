00cd77ed923b309967a340869ff4a5b6a85e417e
lh2966 <bach1bach2@163.com>
2019-12-16 10:31:34 +0800

    add Github and user name

--- src/main/java/com/lhh/hello_spring/controller/AuthorizeController.java
+++ src/main/java/com/lhh/hello_spring/controller/AuthorizeController.java
@@ -1,14 +1,31 @@
 package com.lhh.hello_spring.controller;
 
+import com.lhh.hello_spring.dto.AccessTokenDTO;
+import com.lhh.hello_spring.dto.GithubUser;
+import com.lhh.hello_spring.provider.GithudProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithudProvider githudProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name = "state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("dd48594715f07aa9a49b");
+        accessTokenDTO.setClient_secret("3710c6be1f7622986d6f336766b7922b6a2d7f3f");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:9527/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githudProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githudProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
--- src/main/java/com/lhh/hello_spring/provider/GithudProvider.java
+++ src/main/java/com/lhh/hello_spring/provider/GithudProvider.java
@@ -1,6 +1,8 @@
 package com.lhh.hello_spring.provider;
 
+import com.alibaba.fastjson.JSON;
 import com.lhh.hello_spring.dto.AccessTokenDTO;
+import com.lhh.hello_spring.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -8,18 +10,41 @@ import java.io.IOException;
 
 @Component
 public class GithudProvider {
-    public String gtiAccessToken(AccessTokenDTO accessTokenDTO){
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
         OkHttpClient client = new OkHttpClient();
 
-        RequestBody body = RequestBody.create(JSON, accessTokenDTO);
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
         Request request = new Request.Builder()
-                .url(url)
+                .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            String string =response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return  null;
     }
+
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string =response.body().string();
+            GithubUser githubUser=JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return  null;
+    }
+
 }
