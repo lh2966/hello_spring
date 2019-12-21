376e5dcd082def1103a3f6ce3c6f4eb240d3dbc1
lh2966 <bach1bach2@163.com>
2019-12-21 20:17:31 +0800

    session user xianshi name

--- src/main/java/com/lhh/hello_spring/provider/GithudProvider.java
+++ src/main/java/com/lhh/hello_spring/provider/GithudProvider.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class GithudProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
-
         OkHttpClient client = new OkHttpClient();
 
         RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
@@ -22,6 +21,7 @@ public class GithudProvider {
                 .build();
         try (Response response = client.newCall(request).execute()) {
             String string =response.body().string();
+            //System.out.println(string);
             String token = string.split("&")[0].split("=")[1];
             return token;
         } catch (IOException e) {
@@ -39,7 +39,7 @@ public class GithudProvider {
         try {
             Response response = client.newCall(request).execute();
             String string =response.body().string();
-            GithubUser githubUser=JSON.parseObject(string, GithubUser.class);
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
             return githubUser;
         } catch (IOException e) {
             e.printStackTrace();
